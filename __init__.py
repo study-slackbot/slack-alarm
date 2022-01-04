626dae9338147e3e7debb872c9febdf3be45eab5
cilverjoo <yorkessw@gmail.com>
2022-01-04 21:42:37 +0900

    datepicker, submit 후 스레드에 response 전달 성공, beautifulsoup으로 openapi 파싱 후 df 생성

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 import json
-import re
-import ssl
-import uuid
+import requests
+import threading
 
 from flask import Flask
 from slack import SlackAPI
@@ -16,44 +15,57 @@ from funcy import cached_property
 app = Flask(__name__)
 
 
+
 @app.route('/covid/interactivity', methods=['POST'])
 def interaction() -> str:
     req = request.form.to_dict()
     payload = json.loads(req['payload'])
-    action_type = payload['type'] # block_actions, view_submission, ...
-    trigger_id = payload['trigger_id']
-    action_value = payload['actions'][0]['value'] #get_covid_data, ...
+    for key, value in payload.items():
+        if key == 'response_url':
+            response_url = value
+    payload_type = payload['type']
+    actions = payload['actions'][0]
 
-    if action_value == 'get_covid_data': # button을 눌렀을 때
-        for id, dict in payload['state']['values'].items():
-            if dict.get('get-start-date'):
-                start_date = dict['get-start-date']['selected_date']
-            elif dict.get('get-end-date'):
-                end_date = dict['get-end-date']['selected_date']
+    if payload_type == 'block_actions':
+        if actions['type'] == 'button':
+            for _, value in payload['state']['values'].items():
+                if value.get('get-start-date'):
+                    start_date = value['get-start-date']['selected_date']
+                elif value.get('get-end-date'):
+                    end_date = value['get-end-date']['selected_date']
+                else:
+                    result = {
+                        "response_type": "ephemeral",
+                        "text": "Press submit after picking both dates.",
+                        "attachments": [
+                            {
+                                "text": f'pick date! :('
+                            }
+                        ]
+                    }
+                    requests.post(response_url, json=result)
 
-    result = {
-        'start_date': start_date,
-        'end_date': end_date
-    }
-    print(result)
-    return 'ok'
+        elif actions['type'] == 'datepicker':
+            return 'ok', 200
+
+        result = {
+            "response_type": "ephemeral",
+            "text": "complete",
+            "attachments": [
+                {
+                    "text": f'from {start_date} to {end_date}'
+                }
+            ]
+        }
+        requests.post(response_url, json=result)
+        return 'ok', 200
 
 
 @app.route('/covid', methods=['POST'])
 def test():
     req = request.form.to_dict()
-    print(req)
-    request_text = req['text']
     body = generating_menu()
-    result = {
-        "response_type": "ephemeral",
-        "text": "thread_test",
-        "attachments": [
-            {
-                "text": f'{request_text}'
-            }
-        ]
-    }
+
     return body, 200
 
 
--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-import uuid
-from slack import SlackAPI
+import re
+import requests
+import numpy as np
+import pandas as pd
+import plotly.offline as plyo
+import cufflinks
+from bs4 import BeautifulSoup
 
 
-def test_slack():
-    token = 'xoxb-2873001082209-2860360577010-5TO5dyOLkO6jC2owSqeSXzAC'
-    request_id = str(uuid.uuid4())
-    channel_name = "랜덤"
-    channel_id = SlackAPI(token).get_channel_id(channel_name)
-    message = SlackClient().get_requested_ts(channel_id, request_id)
-    print(message)
+def get_response(end_date):
+    url = f'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?'
+    SERVICE_KEY = 'iq7mlnFcFNOTr5dYWWXp8KNHKM2wM3kbT4c0y+nQele+YvLip6tuF1arCglqWSHsSxWnKYxpHvGUp2h9FGiLyQ=='
+
+    params = {
+        'ServiceKey': SERVICE_KEY,
+        'startCreateDt': 20211201,
+        'endCreateDt': end_date
+    }
+
+    return requests.get(url, params)
+
+
+if __name__ == "__main__":
+    res = get_response(20220101)
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.text, 'xml.parser')
+        decide_cnt_list = []
+        for decidecnt in soup.select('decideCnt'):
+            cnt = re.findall('\d+', str(decidecnt))
+            decide_cnt_list.append(*cnt)
+
+        index = pd.date_range('2021-12-01', periods=len(decide_cnt_list))
+        df = pd.DataFrame(decide_cnt_list, columns=['decideCnt'], index=index)
+
+        plyo.iplot(df.iplot(asFigure=True))
+
+        # result = xmltodict.parse(res.text)
+        # dict = json.loads(json.dumps(result))
+        # print(dict)
